e362ed9c62d2f49eff9fba0aeceb70235b4d6c37
7man7LMYT <67489949+7man7LMYT@users.noreply.github.com>
2021-01-25 22:35:08 -0800

    Changed some lines.
    Changed boot up message, also cleaned some code up.

--- src/main/java/me/stats/CustomStats/CustomStats.java
+++ src/main/java/me/stats/CustomStats/CustomStats.java
@@ -1,32 +1,26 @@
 package me.stats.CustomStats;
 
-import org.bukkit.GameMode;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.HashMap;
+
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public class CustomStats extends JavaPlugin {
     private Logger console;
-    public final HashMap<Player, GameMode> gamemodelist = new HashMap<>();
-    public final HashMap<Player, Integer> taskidlist = new HashMap<>();
     public void onEnable() {
-        getCommand("stats").setExecutor(new StatsCommand(this));
+        Objects.requireNonNull(getCommand("stats")).setExecutor(new StatsCommand(this));
         console = getLogger();
-
-        console.info("Loading configuration...");
+        console.info("Loading config.");
         saveDefaultConfig();
-        console.info("Configuration loaded.");
-		
+        console.info("Config loaded!");
         console.info("Checking for a newer version.");
         new UpdateChecker(this, 88300).getVersion(version -> {
             if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
-                console.info("Your up to date!");
+                console.info("You are up to date! (v1.1.0)");
             } else {
                 console.info("There is a new update available. Download it at https://www.spigotmc.org/resources/customstats.88300/!");
             }
         });
-
         console.info("Successfully started!");
     }
 }
--- src/main/java/me/stats/CustomStats/StatsCommand.java
+++ src/main/java/me/stats/CustomStats/StatsCommand.java
@@ -12,23 +12,21 @@ import java.util.List;
 
 public class StatsCommand implements CommandExecutor, TabExecutor {
     CustomStats customStats;
-
     public StatsCommand(CustomStats customStats) {
         this.customStats = customStats;
     }
-
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
         if (sender instanceof Player) {
             Player p = (Player) sender;
             if (args.length == 1){
                 if (args[0].equals("reload")){
-                    //checks for permission
+                    // Checks for permission
                     if (p.hasPermission("customstats.reload")){
                         customStats.reloadConfig();
                         customStats.saveConfig();
                         customStats.getConfig();
-                        p.sendMessage(ChatColor.BLUE+"Configuration reloaded!");
+                        p.sendMessage(ChatColor.GREEN+"Configuration reloaded!");
                     }
                     else{
                         p.sendMessage(ChatColor.RED+"Usage: /stats");
