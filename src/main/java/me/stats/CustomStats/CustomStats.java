0bcb4658086466c74f4b0567f22b1b80e24bb27b
7man7LMYT <67489949+7man7LMYT@users.noreply.github.com>
2021-02-01 13:36:05 -0800

    Add bStats support.

--- src/main/java/me/stats/CustomStats/CustomStats.java
+++ src/main/java/me/stats/CustomStats/CustomStats.java
@@ -1,5 +1,6 @@
 package me.stats.CustomStats;
 
+import me.stats.CustomStats.bStats.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -17,10 +18,11 @@ public class CustomStats extends JavaPlugin {
         console.info("Loading config.");
         saveDefaultConfig();
         console.info("Config loaded!");
+        new Metrics(this, 10123);
         console.info("Checking for a newer version.");
         new UpdateChecker(this, 88300).getVersion(version -> {
             if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
-                console.info("You are up to date! (v1.1.0)");
+                console.info("You are up to date! (v1.2.1)");
             } else {
                 console.info("There is a new update available. Download it at https://www.spigotmc.org/resources/customstats.88300/!");
             }
